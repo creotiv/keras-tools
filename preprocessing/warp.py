835eb30b95424f61f46c692d68673bc1150d8874
Andrey Nikishaev <creotiv@gmail.com>
2017-11-01 12:34:01 +0200

    Update warp.py

--- preprocessing/warp.py
+++ preprocessing/warp.py
@@ -16,11 +16,11 @@ def unwarp(orig, ench):
     orig_image = cv2.imread(orig, 0)
     orig_image_rgb = cv2.imread(orig)
 
-    surf = cv2.ORB_create()
+    surf = cv2.KAZE_create()
     kp1, des1 = surf.detectAndCompute(ench_image, None)
     kp2, des2 = surf.detectAndCompute(orig_image, None)
 
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+    bf = cv2.BFMatcher()
     matches = bf.knnMatch(des1, des2, k=2)
     
     # store all the good matches as per Lowe's ratio test.
